a46f368db184396d0925428ea961b532a25e45b1
GabrielDavid1 <biocamendeschico@gmail.com>
2025-09-18 21:32:27 -0300

    [Feature]: Apply cors

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 const { ExpressPeerServer } = require("peer");
 const express = require("express");
+const http = require("http");
+const cors = require("cors");
 
 const app = express();
-const server = require("http").createServer(app);
+const server = http.createServer(app);
+
+const allowedOrigins = [
+  "https://talkpix.com.br",
+  "https://www.talkpix.com.br"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 
 const peerServer = ExpressPeerServer(server, {
   debug: true,
   path: "/",
 });
 
+peerServer.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 app.use("/peerjs", peerServer);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 443;
 server.listen(PORT, () => {
   console.log(`PeerJS server rodando na porta ${PORT}`);
 });
